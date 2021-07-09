9e3e11b11c40edd854dd65689da6b758f1301e27
matheesha official <77784129+PercyOfficial@users.noreply.github.com>
2021-07-09 17:22:31 +0530

    Update owner_stuff.py

--- DaisyX/modules/owner_stuff.py
+++ DaisyX/modules/owner_stuff.py
@@ -229,7 +229,7 @@ async def get_event(message):
 
 @register(cmds="stats", is_op=True)
 async def stats(message):
-    text = f"<b>Daisy {DAISY_VERSION} stats</b>\n"
+    text = f"<b>𝑯𝒆𝒓𝒎𝒊𝒐𝒏𝒆 {DAISY_VERSION} 𝒔𝒕𝒂𝒕𝒔</b>\n"
 
     for module in [m for m in LOADED_MODULES if hasattr(m, "__stats__")]:
         text += await module.__stats__()
